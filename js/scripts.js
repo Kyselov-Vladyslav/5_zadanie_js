c6fd88a8218c75fc941326ba2a8f7a435570b59c
Kyselov-Vladyslav <66912506+Kyselov-Vladyslav@users.noreply.github.com>
2022-05-06 00:05:46 +0200

    naprawione

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,23 @@
 "use strict";
 
-const user_surname = ["Kania", "Nowak", "Kowalska", "Maszczykowska"];
-const user_name = ["Piotr", "Adam", "Anna", "Monika"];
+const userSurname = ["Kania", "Nowak", "Kowalska", "Maszczykowska"];
+const userName = ["Piotr", "Adam", "Anna", "Monika"];
 document.querySelector("table").className = "table table-striped";
 document.querySelector("thead").className = "thead-dark";
-let number = 0;
 
-for (let i = 0; i < user_surname.length; i++) {
-  number++;
+for (let i = 0; i < userName.length || i < userSurname.length ; i++) {
   const tr = document.createElement("tr");
-  tr.className = "users_added";
+  tr.className = "users-added";
   const th = document.createElement("th");
   th.scope = "row";
-  const td = document.createElement("td");
+  const tdSurname = document.createElement("td");
+  const tdName = document.createElement("td");
   document.querySelector("tbody").appendChild(tr);
-  document.querySelectorAll(".users_added")[i].prepend(th);
-  document.querySelectorAll(".users_added")[i].appendChild(td);
-  th.textContent = number;
-  td.textContent = user_surname[i];
-}
-
-for (let j = 0; j < user_name.length; j++) {
-  const td = document.createElement("td");
-  document.querySelectorAll(".users_added")[j].appendChild(td);
-  td.textContent = user_name[j];
+  document.querySelectorAll(".users-added")[i].prepend(th);
+  document.querySelectorAll(".users-added")[i].appendChild(tdSurname);
+  document.querySelectorAll(".users-added")[i].appendChild(tdName);
+  th.textContent = i+1;
+  tdSurname.textContent = userSurname[i];
+  tdName.textContent = userName[i]
 }
 
